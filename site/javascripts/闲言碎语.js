8662b3bda4fb36072f41f8e06c6131f58024726f
john-vacuum <2237940662@qq.com>
2026-06-23 19:51:02 +0800

    1.1.5

--- site/javascripts/闲言碎语.js
+++ site/javascripts/闲言碎语.js
@@ -103,7 +103,7 @@ function relativeTime(date) {
   var diff = (now - date.getTime()) / 1000;
   if (diff < 60) return "刚刚";
   if (diff < 3600) return Math.floor(diff / 60) + " 分钟前";
-  if (diff < 86400) return Math.floor(diff / 86400) + " 小时前";
+  if (diff < 86400) return Math.floor(diff / 3600) + " 小时前";
   if (diff < 2592000) return Math.floor(diff / 86400) + " 天前";
   if (diff < 31536000) return Math.floor(diff / 2592000) + " 个月前";
   return Math.floor(diff / 31536000) + " 年前";
